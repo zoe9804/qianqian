77928f779f3e1a791edc8f9eee7e7595ee512f26
zoe9804 <x900124@163.com>
2020-02-28 18:11:02 +0800

    project

---
+++ src/main/java/com/qianqian/qianqian/QianqianApplication.java
@@ -0,0 +1,13 @@
+package com.qianqian.qianqian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QianqianApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QianqianApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/qianqian/qianqian/QianqianApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qianqian.qianqian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QianqianApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
